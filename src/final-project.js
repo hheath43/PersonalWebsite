783e2d98dae7d0d8f8d10d3e6d7b1d19ffc22042
Hannah Heath <heathhan@pdx.edu>
2022-03-07 16:45:05 -0800

    added animations for projs and git symbol

---
+++ src/final-project.js
@@ -0,0 +1,15 @@
+let form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  let nameEle = document.querySelector('#name');
+  let emailEle= document.querySelector('#email');
+  let messageEle = document.querySelector('#message')
+
+  alert('Message submitted!')
+
+  nameEle.value = '';
+  emailEle.value = '';
+  messageEle.value = '';
+
+}
